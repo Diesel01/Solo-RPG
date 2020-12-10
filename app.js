c331cfc50bdccc597ea2c412e52e30f26dc8cec9
Diesel01 <matheusdiesel01@gmail.com>
2020-12-10 09:38:37 -0300

    Added Firebase auth

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+//Firebase auth
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById("whenSignedIn"); 
+const whenSignedOut = document.getElementById("whenSignedOut"); 
+
+const signInBtn = document.getElementById("signInBtn"); 
+const signOutBtn = document.getElementById("signOutBtn"); 
+
+const userDetails = document.getElementById("userDetails"); 
+
+const provider = new firebase.auth.GoogleAuthProvider(); 
+
+signInBtn.onclick = () => auth.signInWithPopup(provider); 
+signOutBtn.onclick = () => auth.signOut(); 
+
+auth.onAuthStateChanged(user =>{
+    if (user) {
+        //signed in 
+        whenSignedIn.hidden = false; 
+        whenSignedOut.hidden = true; 
+        userDetails.innerHTML = `<h3>Olá ${user.displayName}!</h3>`; 
+    } else {
+        //not signed in 
+        whenSignedIn.hidden = true; 
+        whenSignedOut.hidden = false; 
+        userDetails.innerHTML = `<h3>Olá!</h3> <p>Inscreva-se para começar</p>`; 
+    }
+})
+
+// App 
 function question1Conseqs (){
     let optA = document.getElementById("a1"); 
     let optB = document.getElementById("b1");
